6ee18042fbcaffdac5821d855f538c0aa41aa0a3
Owen Norman <75017974+OwenNorman1@users.noreply.github.com>
2024-03-22 14:23:46 +0000

    Add files via upload

---
+++ CalculatorWeb/Controllers/CalcItemsController.cs
@@ -0,0 +1,56 @@
+﻿using CalculatorAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace CalculatorWeb.Controllers
+{
+    public class CalcItemsController : ApiController
+    {
+        SimpleCalculator c = new SimpleCalculator();
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value: " + id.ToString();
+        }
+
+        [HttpPost]
+        public int Add(int a, int b)
+        {
+            return c.Add(a,b);
+        }
+
+        [HttpPost]
+        public int Subtract(int a, int b)
+        {
+            return c.Subtract(a,b);
+        }
+
+        [HttpPost]
+        public int Multiply(int a, int b)
+        {
+            return c.Multiply(a, b);
+        }
+
+        [HttpPost]
+        public int Divide(int a, int b)
+        {
+            return c.Divide(a, b);
+        }
+
+        // POST api/values
+        public string Post([FromBody] string value)
+        {
+            string mess = value + "  " + " RETURN VAL";
+            return mess;
+        }
+    }
+}
